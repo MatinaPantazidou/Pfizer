2a616d6a1b8c521d2ab5e1b11a4af83bf234fc5e
Matina Pantazidou <agapi_pan@yahoo.gr>
2020-11-22 09:48:00 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Test from "./Test";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<Test />, document.getElementById("root"));
+
